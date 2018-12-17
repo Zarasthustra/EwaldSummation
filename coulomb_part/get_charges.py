1f3e29db6734a1f3440704b797517c27b0c7210c
louisl <louislehmann3@googlemail.com>
2018-12-17 17:20:51 +0100

    added  particle charges function

--- coulomb_part/get_charges.py
+++ coulomb_part/get_charges.py
@@ -1,2 +1,7 @@
-def get_charges(particle_kind_vector, charge_)
+def get_charges(particle_kind_vector, charge_a, charge_b):
+    """ gives back a vector of the particle charges
+    """
+    particle_kind_vector = particle_kind()
+    charge_vector = [charge_a  if i==0 else charge_b for i in particle_kind_vector]
+    return charge_vector
     
