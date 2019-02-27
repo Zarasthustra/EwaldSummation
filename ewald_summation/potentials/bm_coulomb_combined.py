be3ca5673552ea57d916f84fd1de423cf63a9142
Yaoyi Chen <29800192+yaoyic@users.noreply.github.com>
2019-02-27 18:25:19 +0100

    reciprocal part and a test in combination, still with incorrect results.

---
+++ ewald_summation/potentials/bm_coulomb_combined.py
@@ -0,0 +1,110 @@
+import numpy as np
+from coulomb_real import CoulombReal
+from coulomb_reciprocal import CoulombReciprocal
+from timeit import default_timer as timer
+
+class FakeWorld:
+    def __init__(self):
+        self.k_C = 1.
+
+class FakeConfig:
+    def __init__(self, n_dim, l_box, n_particles, particle_info):
+        self.n_dim = n_dim
+        self.PBC = True
+        self.l_box = l_box
+        self.n_particles = n_particles
+        self.particle_info = particle_info
+        self.phys_world = FakeWorld()
+        self.particle_types = [
+            # Argon parameter from Rowley, Nicholson and Parsonage, 1975
+            ('Ar', 39.948, 0., 3.405, 0.238), #0
+            # data below are from software MDynaMix
+            # http://www.fos.su.se/~sasha/mdynamix/Examples/nacl.html
+            # water parameter finally from SPC/F model
+            # K TOUKAN AND A.RAHMAN, PHYS. REV. B Vol. 31(2) 2643 (1985)
+            ('OW', 15.999, -0.82, 3.166, 0.155), #1
+            ('HW', 1.0079, 0.41, 0., 0.), #2
+            # NaCl ori ref: https://doi.org/10.1063/1.466363
+            ('Na+', 22.990, 1., 2.35, 0.130), #3
+            ('Cl-', 35.453, -1., 4.40, 0.100) #4
+            ]
+
+'''
+a = lj_pairwise('config*', 2, 3)
+print(a)
+
+a = lj_pairwise(FakeConfig(), 6., 8.)
+print(a.pot_func((1, 0), (np.array([3., 4., 0.]), 5.)))
+print(a.force_func((1, 0), (np.array([3., 0., 0.]), 3.)))
+'''
+def _grid(ns):
+    xx = np.meshgrid(*[np.arange(0, n) for n in ns])
+    X = np.vstack([v.reshape(-1) for v in xx]).T
+    return X
+
+def _intializer_NaCl(n):
+    n_particles = n * n * n
+    n_dim = 3
+    l_box = 4. * np.array([n, n, n])
+    grid=_grid([n, n, n])
+    q = 4. * grid
+    particle_info = grid.sum(axis=1)%2+3
+    return q, particle_info, l_box
+
+q, particle_info, l_box = _intializer_NaCl(16) # 12*12*12 = 1728 particles
+config = FakeConfig(q.shape[1], l_box, q.shape[0], particle_info)
+
+accuracy = 1e-8
+# ratio_real_rec = 5.3 #for 1e-6 accuracy
+ratio_real_rec = 5.5 # for 1e-8 accuracy
+V = config.l_box[0] * config.l_box[1] * config.l_box[2]
+alpha = ratio_real_rec * np.sqrt(np.pi) * (config.n_particles / V / V) ** (1/6)
+REAL_CUTOFF = np.sqrt(-np.log(accuracy)) / alpha
+REC_RESO = int(np.ceil(np.sqrt(-np.log(accuracy)) * 2 * alpha))
+
+a = CoulombReal(config, alpha, REAL_CUTOFF)
+b = CoulombReciprocal(config, alpha, REC_RESO)
+a.set_positions(q)
+b.set_positions(q)
+#for pair in a.pairs:
+#    print(pair)
+# to show the results and finish the jit compiling
+print('MULTI:', a.MULTI)
+print('real cutoff:', REAL_CUTOFF)
+print('reciprocal reso:', REC_RESO)
+print(a.pot + b.pot)
+print(len(a.forces))
+print()
+
+# benchmark
+pasofdp = None
+start = timer()
+for i in range(20):
+    a.set_positions(q)
+    #print(a.pot)
+    pasofdp = a.pot
+stop = timer()
+print('\nreal potential per step:', (stop - start)/20)
+start = timer()
+for i in range(20):
+    b.set_positions(q)
+    #print(a.pot)
+    pasofdp = b.pot
+stop = timer()
+print('\nreciprocal potential per step:', (stop - start)/20)
+
+pasofdp = None
+start = timer()
+for i in range(20):
+    a.set_positions(q)
+    #print(a.pot)
+    pasofdp = a.forces
+stop = timer()
+print('\nreal forces per step:', (stop - start)/20)
+start = timer()
+for i in range(20):
+    b.set_positions(q)
+    #print(a.pot)
+    pasofdp = b.forces
+stop = timer()
+print('\nreciprocal forces per step:', (stop - start)/20)
---
+++ ewald_summation/potentials/coulomb_reciprocal.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+from global_template import globalx
+
+@globalx
+def CoulombReciprocal(config, alpha, rec_reso):
+    # precalc
+    n_dim = config.n_dim
+    assert n_dim == 3, "For other dimensions not implemented."
+    assert config.PBC, "Ewald sum only meaningful for periodic systems."
+    prefactor = config.phys_world.k_C / 2.
+    
+    l_box = np.array(config.l_box)
+    charges = np.empty(config.n_particles)
+    for i in range(config.n_particles):
+        type_i = config.particle_info[i]
+        charges[i] = config.particle_types[type_i][2]
+    
+    m = (1/l_box) * _grid_points_without_center(rec_reso, rec_reso, rec_reso)
+    m_modul_sq = np.linalg.norm(m, axis = 1) ** 2
+    coeff_S = np.exp(-(math.pi / alpha) ** 2 * m_modul_sq) / m_modul_sq
+    v_rec_prefactor = prefactor * 0.5 / math.pi / (l_box[0] * l_box[1] * l_box[2])
+    f_rec_prefactor = -prefactor * charges / (l_box[0] * l_box[1] * l_box[2]) # j and 2pi parts come here
+    v_self = -alpha / math.sqrt(math.pi) * np.sum(charges**2)
+
+    def pot_func(q):
+        m_dot_q = np.matmul(q, np.transpose(m))
+        middle0 = np.pi * 2. * m_dot_q
+        S_m_cos_sum = charges.dot(np.cos(middle0))
+        S_m_sin_sum = charges.dot(np.sin(middle0))
+        S_m_modul_sq = np.square(S_m_cos_sum) + np.square(S_m_sin_sum)
+        v_rec = v_rec_prefactor * np.sum(coeff_S * S_m_modul_sq)
+        # print('new', v_rec)
+        return v_rec + v_self
+
+    def force_func(q):
+        m_dot_q = np.matmul(q, np.transpose(m))
+        middle0 = np.pi * 2. * m_dot_q
+        S_m_cos_parts = charges[:, np.newaxis] * np.cos(middle0)
+        S_m_sin_parts = charges[:, np.newaxis] * np.sin(middle0)
+        S_m_cos_sum, S_m_sin_sum = S_m_cos_parts.sum(axis=0), S_m_sin_parts.sum(axis=0)
+        dS_modul_sq = 2. * S_m_cos_sum * S_m_sin_parts - 2. * S_m_sin_sum * S_m_cos_parts
+        f_rec = f_rec_prefactor[..., None] * (coeff_S * dS_modul_sq).dot(m)
+        return f_rec
+    
+    return pot_func, force_func
+
+def _grid_points_without_center(nx, ny, nz):
+    a, b, c = np.arange(-nx, nx+1), np.arange(-ny, ny+1), np.arange(-nz, nz+1)
+    xx, yy, zz = np.meshgrid(a, b, c)
+    X = np.vstack([xx.reshape(-1), yy.reshape(-1), zz.reshape(-1)]).T
+    return np.delete(X, X.shape[0] // 2, axis=0)
