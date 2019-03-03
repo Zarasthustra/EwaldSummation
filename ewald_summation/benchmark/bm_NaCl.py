Salt condensation (MD) and water clustering (GD). Fix of gradient decent

ewald_summation/benchmark/bm_NaCl.py
@@ -10,7 +10,7 @@ def _grid(ns):
 def _intializer_NaCl(n):
     n_particles = n * n * n
     n_dim = 3
-    l_box = 10. * np.array([n, n, n])
+    l_box = 10. * np.array([2 * n, n, n])
     grid=_grid([n, n, n])
     q = 10. * grid
     v = np.zeros((n_particles, n_dim))
@@ -18,9 +18,9 @@ def _intializer_NaCl(n):
     return q, v, particle_info, l_box
 
 q, v, particle_info, l_box = _intializer_NaCl(4)
-steps = 2000
-test_config = es.SimuConfig(l_box=l_box, PBC=True, particle_info=particle_info, n_steps=steps, timestep=2, temp=1400)
-init = lambda x,y: (q, v)
+steps = 10000
+test_config = es.SimuConfig(l_box=l_box, PBC=True, particle_info=particle_info[1:-1], n_steps=steps, timestep=0.2, temp=2400)
+init = lambda x,y: (q[1:-1], v[1:-1])
 test_md = es.MD(test_config, init, es.step_runners.Langevin(damping=0.1))
 #test_md.add_potential(es.potentials.Water(test_config))
 test_md.add_potential(es.potentials.LJ(test_config, switch_start=5., cutoff=7.))
@@ -30,6 +30,6 @@ test_md.add_potential(es.potentials.Coulomb(test_config))
 test_md.run_all()
 #print(test_md.traj.get_qs())
 qs = test_md.traj.get_qs()
-pdb = es.observables.PdbWriter(test_config, 'NaCl2.pdb')
-for i in range(int(steps/10)):
-    pdb.write_frame(qs[i * 10])
+pdb = es.observables.PdbWriter(test_config, 'NaCl6.pdb', put_in_box=True)
+for i in range(steps//8):
+    pdb.write_frame(qs[i*8])