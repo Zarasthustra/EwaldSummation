e31fba53c6043f90d9146b7f86c0fb57818c3b6f
louisl <louislehmann3@googlemail.com>
2019-03-03 20:47:23 +0100

    a_posteriori g_r implementet + dynamical change the step size in mmc

--- ewald_summation/observables/radial_distribution_function.py
+++ ewald_summation/observables/radial_distribution_function.py
@@ -1,53 +1,117 @@
 import numpy as np
-from itertools import product
-from numba import jit
+from numba import njit
 
-class RadialDistributionFunction:
-    def __init__(self, config, qs):
-        self.qs = qs
-        self.l_box = config.l_box
-        self.n_particles = config.n_particles
-        self.n_dim = config.n_dim
-    def g_r(self):
-        """
-        writes a .xyz format trajectory
-        qs = coordinates 
-        component_a = string for example "Na"
-        component_a = string for example "Cl"
-        """
-        ks = np.array(list(product(range(-1, 2), repeat=self.n_dim)))
-        ks = self.l_box * np.delete(ks, ks.shape[0] // 2, 0)
-        g_r,bins = radial_dist_func(self.qs,ks,self.n_dim,self.n_particles,self.l_box)
-        return g_r,bins
-    
-def distances_not_PBC(q):
+def distances_not_periodic(q):
+    return np.linalg.norm(q[:, None, :] - q[None, :, :], axis = 2)
+
+def distances_periodic(q, l_box):
+    # new implementation
     distance_vectors = q[:, None, :] - q[None, :, :]
-    return distance_vectors
+    np.mod(distance_vectors, l_box, out=distance_vectors)
+    mask = distance_vectors > np.divide(l_box, 2.)
+    distance_vectors += mask * -l_box
+    return np.linalg.norm(distance_vectors, axis = 2)
 
-@jit(parallel=True)
-def radial_dist_func(qs,ks,n_dim,n_particles,l_box):
-    density = n_particles/np.prod(l_box)
-    bin_res = 50
-    r_max = np.min(l_box)
-    bin_width = r_max/bin_res
-    hist,g_r,delta_V = np.zeros(bin_res-1), np.zeros(bin_res-1), np.zeros(bin_res)
-    bins = r_max / bin_res * np.arange(bin_res)
-    tril_vector = np.tril_indices(n_particles,-1)
-    for _ in range(int(0.25*qs.shape[0]),qs.shape[0]):
-        dist= np.linalg.norm(distances_not_PBC(qs[_]%l_box), axis=2)[tril_vector]
-        for k in ks:
-            periodic_dist = np.linalg.norm(distances_not_PBC(qs[_]%l_box)+k,axis=2)[tril_vector]
-            dist = np.append(dist,periodic_dist)
-        #print('dist')
-        #print(dist)
-        #print('qs')
-        #print(qs[_])
-        hist,trash = np.histogram(dist, bins)
-        g_r +=  hist
-    if n_dim==3:
-        delta_V = 2./3. * np.pi * ((bins+bin_width)**3. -bins**3)
-    if n_dim==2:
-        delta_V = 0.5 * np.pi * ((bins+bin_width)**2. -bins**2)
-    delta_V = np.delete(delta_V,-1)
-    bins = np.delete(bins,-1) + 0.5 * bin_width
-    return g_r / density / n_particles / delta_V / (qs.shape[0]-int(0.25*qs.shape[0])), bins
+class RadDistFunc:
+    """
+    input simuconfig, resolution wich means bin size of radii, and p_kinds list
+    with max 3 p_kinds, for example 300 Ar, 200, Na, and 200 Cl, would be
+    [300,200,200]
+    """
+    def __init__(self, config, bin_res, p_kinds):
+        self.n_steps = config.n_steps
+        self.l_box = np.array(config.l_box)
+        self.n_particles = config.n_particles
+        self.p_kinds = p_kinds
+        self.PBC = config.PBC
+        # times rad_func get called
+        self.n_dim = config.n_dim
+        self.r_max = np.min(self.l_box) / 2
+        print(self.r_max)
+        if self.r_max >= 9:
+            self.r_max = 9
+        self.bin_res = bin_res
+        self.bin_width = self.r_max / self.bin_res
+        self.bins = self.bin_width  * np.arange(self.bin_res)    
+        self.p_index = []
+        for _ in range(len(self.p_kinds)):
+            self.p_index = np.append(self.p_index, [_] * self.p_kinds[_]).astype(int)
+        
+    def calc_radial_dist(self, qs, sample_start, sample_rate):
+        """
+        returns array of radial distribution functions, between max 3 Particle Types
+        g_r, g_r_0_0, g_r_1_1, g_r_2_2, g_r_0_1, g_r_0_2, g_r_1_2
+        """
+        count_samples = 0
+        hist_array = np.zeros([6,self.bin_res-1])
+        for step in range(sample_start,self.n_steps):
+            if step % sample_rate == 0 and step >= sample_start:
+                count_samples += 1
+                q = qs[step]
+                if self.PBC == True:
+                    distances = distances_periodic(q, self.l_box)
+                else:
+                    distances = distances_not_periodic(q)
+                dist_array = get_dist_array(distances, self.l_box, self.r_max, self.p_index)
+                hist_array += self.hist_func(dist_array)
+        return self.averaging(hist_array, count_samples)
+
+    def hist_func(self, dist_array):
+        hist = np.zeros([6,self.bin_res-1])
+        for _ in range(6):
+            if len(dist_array[_]) != 0:
+                hist[_], bins = np.histogram(dist_array[_], self.bins)
+        return hist
+    
+    def averaging(self, hist_array, count_samples):
+        """ 
+        averages the sampled data, and norms to density of an ideal gas
+        """
+        delta_V = np.zeros(self.bin_res-1)
+        if self.n_dim==3:
+            self.delta_V = 2./3. * np.pi * ((self.bins+self.bin_width)**3. - self.bins**3)
+        if self.n_dim==2:
+            self.delta_V = 0.5 * np.pi * ((self.bins+self.bin_width)**2. - self.bins**2)
+        if self.n_dim==1:
+            self.delta_V = [0.5*(self.bin_width)] * 2
+        delta_V = np.delete(self.delta_V,-1)
+        real_bins = np.delete(self.bins,-1) + 0.5 * self.bin_width
+        norm_list = [self.p_kinds[0]**2,self.p_kinds[1]**2,self.p_kinds[2]**2, 2*self.p_kinds[0]*self.p_kinds[1]
+                     ,2*self.p_kinds[0]*self.p_kinds[2],2*self.p_kinds[1]*self.p_kinds[2],np.sum(self.p_kinds)**2]
+        g_r = np.zeros([7,len(real_bins)])
+        hist_array = np.append(hist_array, np.sum(hist_array,axis=0)).reshape(7,self.bin_res-1)
+        print(hist_array)
+        for _ in range(7):
+            if np.sum(hist_array[_]) != 0:
+                g_r[_] =  hist_array[_] * np.prod(self.l_box)/ norm_list[_] / delta_V / count_samples
+        return g_r, real_bins
+
+
+@njit
+def get_dist_array(distances, l_box, r_max, p_index):
+    """ 
+    computes histogramm of distances, without neighbourlists
+    """
+    dist_0_0 = []
+    dist_1_1 = []
+    dist_2_2 = []
+    dist_0_1 = []
+    dist_0_2 = []
+    dist_1_2 = []
+    for i in range(len(distances)):
+        for j in range(i+1,len(distances)):
+            actual_dist = distances[i,j]
+            if actual_dist <= r_max:
+                if p_index[i] == 0 and p_index[j] == 0:
+                    dist_0_0.append(actual_dist)
+                if p_index[i] == 1 and p_index[j] == 1:
+                    dist_1_1.append(actual_dist)
+                if p_index[i] == 2 and p_index[j] == 2: 
+                    dist_2_2.append(actual_dist)
+                if p_index[i] == 0 and p_index[j] == 1 or p_index[i] == 1 and p_index[j] == 0:  
+                    dist_0_1.append(actual_dist)       
+                if p_index[i] == 0 and p_index[j] == 2 or p_index[i] == 2 and p_index[j] == 0:  
+                    dist_0_2.append(actual_dist)       
+                if p_index[i] == 2 and p_index[j] == 1 or p_index[i] == 1 and p_index[j] == 2:
+                    dist_1_2.append(actual_dist)
+    return [dist_0_0, dist_1_1, dist_2_2, dist_0_1, dist_0_2, dist_1_2]
--- ewald_summation/step_runners/mc.py
+++ ewald_summation/step_runners/mc.py
@@ -8,18 +8,33 @@ class MMC:
     def init(self, config):
         self.beta = 1. / (config.phys_world.k_B * config.temp)
         self.n_particles, self.n_dim = config.n_particles, config.n_dim
+        self.mc_step = self.step
+        #inital maximal moving particle step
+        self.mc_accepted = 0
+        # counter for accepted moves
         self.potential = None # Storage for last frame potential
 
     def run(self, sum_force, sum_potential, frame, next_frame, step):
         if self.potential is None:
             self.potential = sum_potential(frame.q, step=None)
+            
+        if step % 100 == 0 and step != 0:
+            acc_rate = self.mc_accepted / 100
+            if acc_rate >= 0.6:
+                self.mc_step *= 1.05
+            if acc_rate <= 0.4:
+                self.mc_step *= 0.95
+            self.mc_accepted = 0
+        # dynamically change the acceptance rate to make shure that the phase space 
+        # is accurately sampled. The interval 40-60 % is taken form Allen Tildesley 1989   
         pointToMove = np.random.randint(self.n_particles)
         proposed = frame.q.copy()
-        proposed[pointToMove] += uniform(-self.step, self.step, self.n_dim)
+        proposed[pointToMove] += uniform(-self.mc_step, self.mc_step, self.n_dim)
         propose_potential = sum_potential(proposed, step=None)
         if uniform() < np.exp(self.beta * (self.potential - propose_potential)):
             next_frame.q[:, :] = proposed
             self.potential = propose_potential
+            self.mc_accepted += 1
         else:
             next_frame.q[:, :] = frame.q
         return next_frame
